fea058c40c4f0fe78222ad06f8d85d50702db313
Pramod Daware <dawarepramod4@gmail.com>
2023-04-25 11:02:56 +0530

    chANGES

--- routes/api.js
+++ routes/api.js
@@ -7,7 +7,7 @@ router.get("/question", async function (req, res) {
         const questions = await Question.find({});
         const msg = {
             questions: questions,
-            msg: "This Ayush's project",
+            msg: "The Ayush's project",
         };
         res.json(msg);
     } catch (e) {
